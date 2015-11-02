252ec554cbe0cefaffad85687cf080f41164771f
Jason Karns <jason@karns.name>
2015-11-02 11:52:17 -0500

    use BATS_MOCK_TMPDIR and BATS_MOCK_BINDIR internally

--- stub.bash
+++ stub.bash
@@ -1,25 +1,28 @@
-PATH="$BATS_TMPDIR/bin:$PATH"
+BATS_MOCK_TMPDIR="${BATS_TMPDIR}"
+BATS_MOCK_BINDIR="${BATS_MOCK_TMPDIR}/bin"
+
+PATH="$BATS_MOCK_BINDIR:$PATH"
 
 stub() {
   local program="$1"
   local prefix="$(echo "$program" | tr a-z- A-Z_)"
   shift
 
-  export "${prefix}_STUB_PLAN"="${BATS_TMPDIR}/${program}-stub-plan"
-  export "${prefix}_STUB_RUN"="${BATS_TMPDIR}/${program}-stub-run"
+  export "${prefix}_STUB_PLAN"="${BATS_MOCK_TMPDIR}/${program}-stub-plan"
+  export "${prefix}_STUB_RUN"="${BATS_MOCK_TMPDIR}/${program}-stub-run"
   export "${prefix}_STUB_END"=
 
-  mkdir -p "${BATS_TMPDIR}/bin"
-  ln -sf "${BASH_SOURCE[0]%stub.bash}binstub" "${BATS_TMPDIR}/bin/${program}"
+  mkdir -p "${BATS_MOCK_BINDIR}"
+  ln -sf "${BASH_SOURCE[0]%stub.bash}binstub" "${BATS_MOCK_BINDIR}/${program}"
 
-  touch "${BATS_TMPDIR}/${program}-stub-plan"
-  for arg in "$@"; do printf "%s\n" "$arg" >> "${BATS_TMPDIR}/${program}-stub-plan"; done
+  touch "${BATS_MOCK_TMPDIR}/${program}-stub-plan"
+  for arg in "$@"; do printf "%s\n" "$arg" >> "${BATS_MOCK_TMPDIR}/${program}-stub-plan"; done
 }
 
 unstub() {
   local program="$1"
   local prefix="$(echo "$program" | tr a-z- A-Z_)"
-  local path="${BATS_TMPDIR}/bin/${program}"
+  local path="${BATS_MOCK_BINDIR}/${program}"
 
   export "${prefix}_STUB_END"=1
 
@@ -27,6 +30,6 @@ unstub() {
   "$path" || STATUS="$?"
 
   rm -f "$path"
-  rm -f "${BATS_TMPDIR}/${program}-stub-plan" "${BATS_TMPDIR}/${program}-stub-run"
+  rm -f "${BATS_MOCK_TMPDIR}/${program}-stub-plan" "${BATS_MOCK_TMPDIR}/${program}-stub-run"
   return "$STATUS"
 }
